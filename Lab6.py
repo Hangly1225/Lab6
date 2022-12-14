691c489241a380c83f92a3de573b50f9162b8479
Hang Ly <hanglyu2002@gmail.com>
2022-12-14 08:05:22 +0300

    Update all

--- Lab6.py
+++ Lab6.py
@@ -1,14 +1,14 @@
-import math
+def simple_calculator(string):
+    try:
+        return eval(string, {}, {})
+    except:
+        return 'ERR'
 
-def add(a: int, b: int) -> int:
-    return a + b
+def main():
+    equation = input()
+    while equation:
+        print(simple_calculator(equation))
+        equation = input()
 
-def subtract(a: int, b: int) -> int:
-    return a - b
-
-def multiply(a: int, b: int) -> int:
-    return a * b
-
-def divide(a: int, b: int) -> float:
-    if(a != 0) and (b != 0): return a / b
-    else: return 'Error'
+if __name__ == '__main__':
+    main()
