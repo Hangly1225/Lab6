b7d2ec2e3f1b59f87029a13c68c9f31672a91a72
Hangly1225 <113439346+Hangly1225@users.noreply.github.com>
2022-12-14 11:33:58 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import Calculator
+from calc import Calculator
 
 
 class TestCalculator(unittest.TestCase):
@@ -8,17 +8,17 @@ class TestCalculator(unittest.TestCase):
         n.calculator = Calculator()
 
     def test_add(n):
-        self.assertEqual(self.calculator.add(9, 7), 16)
+        n.assertEqual(n.calculator.add(9, 7), 16)
 
     def test_subtract(n):
-        self.assertEqual(self.calculator.subtract(112, 22), 90)
+        n.assertEqual(n.calculator.subtract(112, 22), 90)
 
     def test_multiply(n):
-        self.assertEqual(self.calculator.multiply(4, 9), 45)
+        n.assertEqual(n.calculator.multiply(4, 9), 45)
 
     def test_divide(n):
-        self.assertEqual(self.calculator.divide(20, 4), 5)
+        n.assertEqual(n.calculator.divide(20, 4), 5)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.calc()
