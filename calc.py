25e7e96673c3b139ba9fd1ff995a66bafe366319
Hangly1225 <113439346+Hangly1225@users.noreply.github.com>
2022-12-14 11:01:03 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
 class Calculator:
-    def __init__(self):
+    def __init__(n):
         pass
     
-    def add(self, a, b):
+    def add(n, a, b):
         return a + b
     
-    def multiply(self, a, b):
+    def multiply(n, a, b):
         return a * b
     
-    def subtract(self, a, b):
+    def subtract(n, a, b):
         return x1 - b
 
-    def divide(self, a, b):
+    def divide(n, a, b):
         if a != 0:
             return a / b
         else 
